fcf1d6f6ed753410115e07da5e2b6c4719a13de3
Nael WENTZLER <wtznael@gmail.com>
2022-05-06 11:29:40 +0200

    Update IEquipeMatchRepository.java

--- src/main/java/projetFilRouge/repository/IEquipeMatchRepository.java
+++ src/main/java/projetFilRouge/repository/IEquipeMatchRepository.java
@@ -3,7 +3,6 @@ package projetFilRouge.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import projetFilRouge.model.EquipeMatch;
@@ -13,7 +12,7 @@ import projetFilRouge.model.EquipeMatch;
 public interface IEquipeMatchRepository extends JpaRepository<EquipeMatch,Long> {
 
 	
-	List<EquipeMatch> findAllByEquipe(@Param("id") Long idEquipe);
+	List<EquipeMatch> findAllByEquipe(Long idEquipe);
 
-	List<EquipeMatch> findAllByMatch(@Param("id") Long idMatch);
+	List<EquipeMatch> findAllByMatchId(Long matchId);
 }
