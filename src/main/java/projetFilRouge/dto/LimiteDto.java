331055946389a93b39112b41518d6f8d39d41c04
mafeg87 <104212170+mafeg87@users.noreply.github.com>
2022-05-15 18:30:52 +0200

    date pattern pour limiteDto

--- src/main/java/projetFilRouge/dto/LimiteDto.java
+++ src/main/java/projetFilRouge/dto/LimiteDto.java
@@ -2,12 +2,15 @@ package projetFilRouge.dto;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 public class LimiteDto {
 
 	private Long id;
 	
 	private Double valeur;
-
+    
+	@JsonFormat(/* shape = JsonFormat.Shape.STRING, */ pattern = "dd/MM/yyyy")
 	private Date duree;
 			
 	public LimiteDto() {
